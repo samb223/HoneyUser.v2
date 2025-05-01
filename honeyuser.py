bab0f714e104f6734b3017fd34a2b9767ad871a5
samb223 <109436588+samb223@users.noreply.github.com>
2025-05-01 02:10:57 -0400

    Create honeyuser.py

---
+++ honeyuser.py
@@ -0,0 +1,55 @@
+
+import os
+import platform
+import time
+import yaml
+from platform import system
+from utils.helpers import load_config
+from platform import linux, windows, macos
+from activity.simulator import simulate_activity
+from monitor.detector import Monitor
+from alert.logger import Logger
+from alert.emailer import Emailer
+
+
+def get_platform_module():
+    if system() == "Windows":
+        return windows
+    elif system() == "Linux":
+        return linux
+    elif system() == "Darwin":
+        return macos
+    else:
+        raise NotImplementedError("Unsupported OS")
+
+
+def main():
+    config = load_config("config.yaml")
+    platform_module = get_platform_module()
+
+    # Create decoy user
+    if not platform_module.user_exists(config['user']['name']):
+        platform_module.create_user(config['user'])
+
+    # Simulate activity
+    simulate_activity(config)
+
+    # Monitor
+    monitor = Monitor(config)
+    monitor.start()
+
+    # Alert/logging setup
+    logger = Logger(config)
+    emailer = Emailer(config)
+
+    while True:
+        events = monitor.check_events()
+        for event in events:
+            logger.log_event(event)
+            if config['alerts']['email_enabled']:
+                emailer.send_alert(event)
+        time.sleep(10)
+
+
+if __name__ == "__main__":
+    main()
